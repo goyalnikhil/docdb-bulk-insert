ce29fee427adf75be3db2511a9cdc6f810527987
Nikhil Goyal <34941668+goyalnikhil@users.noreply.github.com>
2024-10-23 02:18:19 -0400

    Changed flow to use a single input file.

--- bulk-insert.py
+++ bulk-insert.py
@@ -1,41 +1,68 @@
-import sys
+import time
 import json
-import os
 import argparse
-import math
 import logging
-import multiprocessing
-import threading
 import pymongo
-import boto3
+import lzma
 
+#logging = logging.getLogger(__name__)
 
-def handler_function() :
-    print("hello world")
+def loadJsonFromFile(args):
+    #Connect to DocumentDB
+    client = pymongo.MongoClient(args.uri)
+    db = client[args.database]
+    collection = db[args.collection]
+    #Load JSON string from a file on disk
+    inputFile = open(args.input_file, 'r')
+    json_data = json.load(inputFile)
+    totalDocs = len(json_data['AddOrUpdate'])
 
-def checkS3Access(args):
-    print("checking S3")
+    logging.info(f'{args.input_file},{totalDocs},')
+    logging.info('inserts in batch,time to insert')
 
-def loadJsonFromFile(args, fileName):
-    #function to load JSON string from a file on disk
-    with open(fileName, 'r') as file:
-        data = json.load(file)
-        print(data)
+    thisBatchIns = 0
+    insList = []
+    batchInsertTime = 0
+
+    for doc in json_data['AddOrUpdate']:
+        insDoc = {}
+        insDoc["_id"] = doc["documentId"]
+        insDoc["OrderingId"] = int(time.time())
+        rawDoc = json.dumps(doc)
+        if args.compress:
+            insDoc["Raw"] = lzma.compress(rawDoc.encode('utf-8'))
+        else:
+            insDoc["Raw"] = rawDoc
+        insDoc["ExtraFields"] = {}
+        insDoc["Deleted"] = False
+        insList.append(pymongo.InsertOne(insDoc))
+        thisBatchIns += 1
+        if thisBatchIns == args.batch_size:
+            batchStartTime = time.time()
+            result = collection.bulk_write(insList, ordered=True)
+            batchInsertTime += (time.time() - batchStartTime) * 1000
+            logging.info(f'{thisBatchIns},{batchInsertTime}')
+            insList = []
+            thisBatchIns = 0
+    if len(insList) > 0:
+        batchStartTime = time.time()
+        result = collection.bulk_write(insList, ordered=True)
+        batchInsertTime += (time.time() - batchStartTime) * 1000
+        logging.info(f'{len(insList)},{batchInsertTime}')
+
+    logging.info(f'{args.input_file},{totalDocs},{batchInsertTime}')
 
 if __name__ == "__main__":
-    handler_function()
-
+    logging.basicConfig(filename='bulk-insert.log', format='%(asctime)s,%(message)s', datefmt='%m-%d-%Y %I:%M:%S', level=logging.INFO)
     argParser = argparse.ArgumentParser(description="Bulk data insert")
 
     argParser.add_argument('--uri', required=True, type=str, help='DocDB connecton string URI')
     argParser.add_argument('--database', required=True, type=str, help='Database to use')
     argParser.add_argument('--collection', required=True, type=str, help='Collection to use')
     argParser.add_argument('--batch-size', required=True, type=int, help='Number of documents to insert per batch')
-    argParser.add_argument('--bucket-name', required=True, type=str, help='S3 bucket URI fo the documents')
-    argParser.add_argument('--threads', required=True, type=int, help='Number of threads')
+    argParser.add_argument('--input-file', required=True, type=str, help='Input file with JSON data')
+    argParser.add_argument('--compress', required=False, action='store_true', help='Compress the raw JSON before storing in the Raw attribute')
 
     args = argParser.parse_args()
 
-    checkS3Access(args)
-
-    loadJsonFromFile(args, "test.json")
+    loadJsonFromFile(args)
